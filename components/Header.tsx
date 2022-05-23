c57b0507918f4fa9f7b0a020764947b9f179e070
Mogakamo <mogaka.amo254@gmail.com>
2022-05-23 10:52:18 +0300

    Added the reddit logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
     <div>
-      I am a Header
+      <div className='relative h-10 w-20'>
+        <Image src="https://links.papareact.com/fqy" objectFit='contain' layout="fill" />
+      </div>
     </div>
   )
 }
