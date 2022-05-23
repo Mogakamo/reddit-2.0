fae108216a783e944e7be2aaeb47c77afd29b8c4
Mogakamo <mogaka.amo254@gmail.com>
2022-05-23 16:52:33 +0300

    commit

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,8 +1,28 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
+import Image from 'next/image'
 
-const Avatar = () => {
+type Props = {
+  seed?: string
+  large?: boolean
+}
+
+const Avatar = ({ seed, large }: Props) => {
+  const { data: session } = useSession()
   return (
-    <div>Avatar</div>
+    <div
+      className={`relative h-10 w-10 overflow-hidden rounded-full border-gray-300 bg-white ${
+        large && 'h-20 w-20'
+      }`}
+    >
+      <Image
+        className=""
+        layout="fill"
+        src={`https://avatars.dicebear.com/api/open-peeps/${
+          session?.user?.name || 'placeholder'
+        }.svg`}
+      />
+    </div>
   )
 }
 
--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,20 +1,27 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import Avatar from './Avatar'
+import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
 
 const PostBox = () => {
   const { data: session } = useSession()
 
   return (
-    <form>
-      <div className='flex items-center space-x-3'>
+    <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
+      <div className="flex items-center space-x-3">
         {/* Avatar */}
-
+        <Avatar large />
         <input
           type="text"
           className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
           disabled={!session}
-          placeholder={session? 'Create a post by entering a title!' : 'Signin to post!'}
+          placeholder={
+            session ? 'Create a post by entering a title!' : 'Signin to post!'
+          }
         />
+
+          <PhotographIcon className={'h-6 cursor-pointer text-gray-300'} />
+        <LinkIcon className="h-6 w-6 text-gray-300" />
       </div>
     </form>
   )
