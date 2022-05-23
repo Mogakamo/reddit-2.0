a902a0c8a6ca93895a179e659d6f60fe7a507c8d
Mogakamo <mogaka.amo254@gmail.com>
2022-05-23 11:08:08 +0300

    working on the header icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,30 @@
 import Image from 'next/image'
 import React from 'react'
+import { MenuIcon, ChevronDownIcon, HomeIcon, SearchIcon } from "@heroicons/react/solid"
+import { BellIcon, ChatIcon, GlobeIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon } from "@heroicons/react/outline"
 
 const Header = () => {
-  return (
-    <div>
-      <div className='relative h-10 w-20'>
-        <Image src="https://links.papareact.com/fqy" objectFit='contain' layout="fill" />
-      </div>
-    </div>
-  )
+    return (
+        <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
+            <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
+                <Image src="https://links.papareact.com/fqy" objectFit='contain' layout="fill" />
+            </div>
+
+            <div className='flex items-center mx-7 xl:min-w-[300px]'>
+                <HomeIcon className='h-5 w-5' />
+                <p className='ml-2 hidden lg:inline flex-1'>Home</p>
+                <ChevronDownIcon className='h-5 w-5' />
+            </div>
+
+            {/* Searchbox */}
+            <form className='flex flex-1 items-center space-x-2 border 
+            border-gray-200 rounded-sm bg-gray-100 px-3 py-1 '>
+                <SearchIcon className='h-6 w-6 text-gray-400' />
+                <input type="text" className='flex-1 bg-transparent outline-none' placeholder='Search Reddit' />
+                <button type='submit' hidden />
+            </form>
+        </div>
+    )
 }
 
 export default Header
