dfbc186965d19ef6a24ab320b8fd97c0aa706ba8
Mogakamo <mogaka.amo254@gmail.com>
2022-05-24 08:58:40 +0300

    commit

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -2,9 +2,25 @@ import { useSession } from 'next-auth/react'
 import React from 'react'
 import Avatar from './Avatar'
 import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  postTitle: string
+  postBody: string
+  postImage: string
+  subreddit: string
+}
 
 const PostBox = () => {
   const { data: session } = useSession()
+  const { imageBoxOpen, setImageBoxOpen } = React.useState<boolean>(false)
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>()
 
   return (
     <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
@@ -12,6 +28,7 @@ const PostBox = () => {
         {/* Avatar */}
         <Avatar large />
         <input
+          {...register('postTitle', { required: true })}
           type="text"
           className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
           disabled={!session}
@@ -20,9 +37,38 @@ const PostBox = () => {
           }
         />
 
-          <PhotographIcon className={'h-6 cursor-pointer text-gray-300'} />
-        <LinkIcon className="h-6 w-6 text-gray-300" />
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 cursor-pointer text-gray-300 ${imageBoxOpen && 'text-blue-500'}`}
+        />
+        <LinkIcon className="w -6 h-6 text-gray-300" />
       </div>
+
+      {!!watch('postTitle') && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('postBody')}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          <div className="flex flex-col py-2">
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Subreddit:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register('subreddit')}
+                type="text"
+                placeholder="i.e reactjs"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </form>
   )
 }
